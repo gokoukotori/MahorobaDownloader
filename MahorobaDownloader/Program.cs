a407d22d4c6d9c5f4fb07d87358b2efae9cff481
五香琴理 <25696365+gokoukotori@users.noreply.github.com>
2020-09-01 22:50:55 +0900

    エチエチエッチ絵取得処理実装

--- MahorobaDownloader/Program.cs
+++ MahorobaDownloader/Program.cs
@@ -91,9 +91,24 @@ namespace MahorobaDownloader
 					{
 						binary = GetStandingPictureBinary(item.PictureId);
 					}
+					catch (WebException ex)
+					{
+						HttpWebResponse response = (HttpWebResponse)ex.Response;
+						if (response.StatusCode == HttpStatusCode.NotFound)
+						{
+							logger.Error("エラーが発生。立ち絵画像が存在しない、もしくはキャラクター画像IDは割り振られているが実装されていない可能性があります。");
+						}
+						else
+						{
+							logger.Error("エラーが発生。原因は不明です。");
+						}
+						logger.Error(ex.Message);
+						logger.Error(ex.StackTrace);
+						continue;
+					}
 					catch (Exception ex)
 					{
-						logger.Error("エラーが発生。立ち絵画像が存在しない、もしくはキャラクター画像IDは割り振られているが実装されていない可能性があります。");
+						logger.Error("エラーが発生。原因は不明です。");
 						logger.Error(ex.Message);
 						logger.Error(ex.StackTrace);
 						continue;
@@ -101,6 +116,51 @@ namespace MahorobaDownloader
 					FileUtils.WriteBinaryToFile(Path.Combine(OUTPUT_CHARACTERS_PATH, item.Name,"立ち絵_" + item.PictureId + ".png"), binary);
 				}
 				logger.Info("立ち絵取得終了");
+
+
+				logger.Info("エッチな絵取得開始");
+				foreach (var item in characters)
+				{
+					if (File.Exists(Path.Combine(OUTPUT_CHARACTERS_PATH, item.Name, "エッチな絵_" + item.PictureId + "_1" + ".png")))
+					{
+						logger.Info("エッチな絵取得済のためスキップ キャラクター画像ID: " + item.PictureId);
+						continue;
+					}
+
+					logger.Info("エッチな絵取得中 キャラクター画像ID: " + item.PictureId);
+					Dictionary<string,byte[]> binary;
+					try
+					{
+						binary = GetCharacterEroPictureBinary(item.PictureId);
+					}
+					catch (WebException ex)
+					{
+						HttpWebResponse response = (HttpWebResponse)ex.Response;
+						if (response.StatusCode == HttpStatusCode.NotFound)
+						{
+							logger.Error("エラーが発生。エッチ過ぎな絵が存在しない、もしくはキャラクター画像IDは割り振られているが実装されていない可能性があります。");
+						}
+						else
+						{
+							logger.Error("エラーが発生。原因は不明です。");
+						}
+						logger.Error(ex.Message);
+						logger.Error(ex.StackTrace);
+						continue;
+					}
+					catch (Exception ex)
+					{
+						logger.Error("エラーが発生。原因は不明です。");
+						logger.Error(ex.Message);
+						logger.Error(ex.StackTrace);
+						continue;
+					}
+					foreach (var eroImg in binary)
+					{
+						FileUtils.WriteBinaryToFile(Path.Combine(OUTPUT_CHARACTERS_PATH, item.Name, "エッチな絵_" + item.PictureId + "_" + eroImg.Key + ".png"), eroImg.Value);
+					}
+				}
+				logger.Info("エッチな絵取得終了");
 
 			}
 			catch(Exception ex)
@@ -169,6 +229,60 @@ namespace MahorobaDownloader
 			return binaries;
 		}
 
+
+		#endregion
+
+
+		#region エチエチエッチ絵処理関連
+		public static Dictionary<string,byte[]> GetCharacterEroPictureBinary(string characterPictureId)
+		{
+			logger.Debug("エッチな絵のバイナリ取得開始");
+			var binaries = new Dictionary<string, byte[]>();
+			using (var client = new WebClient())
+			{
+				for (var index = 1; ; index++)
+				{
+					logger.Debug("エッチ過ぎな絵のバイナリ取得　 Resources: " + "hero/cg/" + characterPictureId + "_" + index + ".png");
+					var hash = MD5ConvertTo("hero/cg/" + characterPictureId + "_" + index + ".png");
+					try
+					{
+						var binary = client.DownloadData(BASE_URL + hash + ".png");
+						binaries.Add(index.ToString(), binary);
+
+					}
+					catch (WebException ex)
+					{
+						HttpWebResponse response = (HttpWebResponse)ex.Response;
+						if (index == 1 || response.StatusCode != HttpStatusCode.NotFound) throw;
+						break;
+					}
+					catch (Exception ex)
+					{
+						throw;
+					}
+					logger.Debug("エッチ過ぎな絵のバイナリ取得② Resources: " + "hero/cg/" + characterPictureId + "_" + index + "_c.png");
+					hash = MD5ConvertTo("hero/cg/" + characterPictureId + "_" + index + "_c.png");
+					try
+					{
+						var binary = client.DownloadData(BASE_URL + hash + ".png");
+						binaries.Add(index + "_c", binary);
+
+					}
+					catch (WebException ex)
+					{
+						HttpWebResponse response = (HttpWebResponse)ex.Response;
+						if (response.StatusCode != HttpStatusCode.NotFound) throw;
+					}
+					catch (Exception ex)
+					{
+						throw;
+					}
+				}
+
+				logger.Debug("エッチな絵のバイナリ取得終了");
+				return binaries;
+			}
+		}
 
 		#endregion
 
